a23bd36bd3670cb1349a511d0e783345c287549b
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 19:09:43 +0530

    [Prabhu] Add: Even or Odd

--- BasicAndFunctionalPrograms/Program.cs
+++ BasicAndFunctionalPrograms/Program.cs
@@ -5,5 +5,22 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Basic and Functional Programs");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nEnter your choice \n1.Even or Odd \n2.Exit");
+            Console.WriteLine("Enter option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    EvenOrOdd evenorOdd = new EvenOrOdd();
+                    evenorOdd.EvenOdd();
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
