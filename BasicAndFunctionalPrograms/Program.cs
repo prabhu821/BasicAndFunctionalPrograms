06bbba5970691e6ed9eedebf6697dcf5624ed94f
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 19:50:33 +0530

    [Prabhu] Add: Swap two Numbers

--- BasicAndFunctionalPrograms/Program.cs
+++ BasicAndFunctionalPrograms/Program.cs
@@ -9,7 +9,7 @@ public class Program
         while (flag)
         {
             Console.WriteLine("\nEnter your choice \n1.Even or Odd \n2.Factors \n3.Flip Coin \n4.Harmonic Number \n5.Largest Number " +
-                "\n6.Leap Year \n7.Power of Two \n8.Quotient And Remainder \n9.Exit");
+                "\n6.Leap Year \n7.Power of Two \n8.Quotient And Remainder \n9.Swap Two Numbers \n10.Exit");
             Console.WriteLine("Enter option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -45,6 +45,10 @@ public class Program
                 case 8:
                     QuotientAndRemainder quotientAndRemainder = new QuotientAndRemainder();
                     quotientAndRemainder.FindValue();
+                    break;
+                case 9:
+                    SwapTwoNumbers swapNumber = new SwapTwoNumbers();
+                    swapNumber.Swap();
                     break;
                 default:
                     flag = false;
